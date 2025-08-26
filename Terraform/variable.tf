828b6e7c4f8deebd70967bb4313784066181f116
Kamal201999 <vrakamalesh@gmail.com>
2025-08-26 18:44:32 +0530

    Changed back

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,14 +1,15 @@
 variable "key_name" {
-  description = "Name for the EC2 key pair"
+  description = "Key pair name"
   type        = string
 }
 
-variable "public_key_path" {
-  description = "Path to the public key file"
+variable "public_key" {
+  description = "Public key for SSH access"
   type        = string
 }
 
-variable "private_key_path" {
-  description = "Private key contents for connecting EC2"
+variable "private_key" {   # 👈 FIX: changed from file path → raw string
+  description = "Private key contents for connecting to EC2"
   type        = string
+  sensitive   = true
 }
